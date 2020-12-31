35f80767b23c02f9c99b8ebd2ca8cd43a56cd36b
Antonio Astorino <antonio.astorino.phd@gmail.com>
2020-12-31 16:57:33 +0100

    Test with repetition counter

--- keyboard.ino
+++ keyboard.ino
@@ -5,12 +5,11 @@
 #define COLS 1
 #define UP 0
 #define DOWN 1
-#define REPEAT 2
+#define REP 2
 #define COUNT_BEFORE_REPEAT 3
 
 uint8_t buf[8]          = {0}; /* Keyboard report buffer */
 uint8_t state_buf[2][1] = {UP};
-uint8_t repeat_count    = COUNT_BEFORE_REPEAT;
 
 void setup() {
     Serial.begin(9600);
@@ -27,32 +26,45 @@ void setup() {
 void loop() {
     for (int row = 0; row < ROWS; row++) {
         PORTC = 1 << row; // Turn on one output at a time in sequence
-        for (int col = 2; col < 2 + COLS; col++) {
-            bool reading          = digitalRead(col);
-            uint8_t* button_state = &state_buf[row][col];
-
-            if (*button_state == DOWN && repeat_count == COUNT_BEFORE_REPEAT) {
-                Serial.write("PRESSED\n");
-            } else if (*button_state == REPEAT) {
-                Serial.write("REPEAT\n");
+        for (int col = 0; col < COLS; col++) {
+            bool reading = digitalRead(col + 2);
+            delay(500);
+            if (state_buf[row][col] == DOWN) {
+                Serial.write("Row: ");
+                Serial.write(65 + row);
+                Serial.write(" PRESSED ");
+            } else if (state_buf[row][col] == REP) {
+                Serial.write("Row: ");
+                Serial.write(65 + row);
+                Serial.write(" REPEAT ");
+            } else if (state_buf[row][col] == UP) {
+                Serial.write("Row: ");
+                Serial.write(65 + row);
+                Serial.write(" UP ");
             } else {
-                Serial.write("UP\n");
+                Serial.write("Row: ");
+                Serial.write(65 + row);
+                Serial.write(" HELD ");
             }
 
             if (reading) {
-                if (repeat_count == 0) {
-                    *button_state = REPEAT;
-                } else {
-                    *button_state = DOWN;
-                    repeat_count--;
+                if (state_buf[row][col] == UP) {
+                    // Push down only the just pushed button
+                    state_buf[row][col] = DOWN;
+                } else if (state_buf[row][col] == DOWN) {
+                    state_buf[row][col] = REP + COUNT_BEFORE_REPEAT;
+                } else if (state_buf[row][col] > REP) {
+                    state_buf[row][col]--;
                 }
             } else {
-                *button_state = UP;
-                repeat_count  = COUNT_BEFORE_REPEAT;
+                state_buf[row][col] = UP;
             }
-            delay(500);
+            Serial.write(" - Repeat count: ");
+            Serial.write(48 - REP + state_buf[row][col]);
+            Serial.write(" - ");
         }
     }
+    Serial.write("\n");
 }
 
 void releaseKey() {
